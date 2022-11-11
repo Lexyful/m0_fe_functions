e22f542a113a71ede06754bc780761143661c55d
Alexys Jordan <alexyskjordan@gmail.com>
2022-11-10 21:28:32 -0700

    Add my functions

--- functions.js
+++ functions.js
@@ -1,22 +1,81 @@
-// 1: Write a function named printGreeting that prints a simple greeting message, the same one, every time it is called. Call this function 3 times.
+// // 1: Write a function named printGreeting that prints a simple greeting message, the same one, every time it is called. Call this function 3 times.
+// function printGreeting() {
+//     console.log("Howdy")
+//     console.log("Ello govna")
+//     console.log("Toppa the mornin")
+// }
+// printGreeting()
+
+// // 2: Write a function that accepts 1 argument. The function should console.log a sentence that interpolates the data passed in.
+// function printGreeting(name) {
+//     console.log(`Ello there, ${name}!`)
+
+// }
+// printGreeting("Shane")
+
+// // 3: Write a function that has 3 parameters: a string and two numbers. The String will be the name of a company, and the numbers will represent the minimum and maximum of a pay range for a posted job. The function should print out a sentence that includes the name of the company and the range itself (if the numbers passed in are 90000 and 110000, the pay range is 20000).
+// function helpNeeded(lowest, highest){
+//     var range = highest - lowest
+//     return range
+// }
+// console.log(helpNeeded(800, 1200));
+// console.log(`The range for the job is ${helpNeeded(800, 1200)}.`)
 
 
-// 2: Write a function that accepts 1 argument. The function should console.log a sentence that interpolates the data passed in.
+// var isOpen = true;
 
-
-// 3: Write a function that has 3 parameters: a string and two numbers. The String will be the name of a company, and the numbers will represent the minimum and maximum of a pay range for a posted job. The function should print out a sentence that includes the name of the company and the range itself (if the numbers passed in are 90000 and 110000, the pay range is 20000).
-
-
+// if (isOpen === true) {
+//   console.log("Let's go!");
+// } else {
+//   console.log("Oops. Looks like that store is closed now.");
+// }
 // 4: Write a function that satifies the following interaction pattern:
 
-checkStock(4, "Coffee");
-// => "Coffee is stocked"
+// var coffeeBeforeShipment = 0;
+// var coffeeAfterShipment = 4;
+// var totalCoffee = add(coffeeBeforeShipment, coffeeAfterShipment);
 
-checkStock(3, "Tortillas");
-// => "Tortillas - running LOW"
+// console.log(`We have ${totalCoffee} pallets of coffee`)
+//   // checkStock(4, "Coffee")
+// // => "Coffee is stocked"
 
-checkStock(0, "Cheese");
-// => "Cheese - OUT of stock!"
+// function add(num1, num2) {
+//   var sum = num1 + num2;
+//   return sum;
+// }
 
-checkStock(1, "Salsa");
-// => "Salsa - running LOW"
+// var morningCommuteMinutes = 18;
+// var afternoonCommuteMinutes = 30;
+// var totalCommuteMinutes = add(morningCommuteMinutes, afternoonCommuteMinutes);
+
+// console.log(`I spend a total of ${totalCommuteMinutes} minutes driving to work each day.`)
+// checkStock(3, "Tortillas");
+// // => "Tortillas - running LOW"
+
+// checkStock(0, "Cheese");
+// // => "Cheese - OUT of stock!"
+
+// checkStock(1, "Salsa");
+// // => "Salsa - running LOW"
+
+function checkStock(amount, ingredient) {
+    if(amount === 4) {
+      console.log(`${ingredient} is stocked.`)
+    }
+    if(amount === 3) {
+      console.log (`${ingredient} is running LOW!`)
+    }
+    if(amount === 0) {
+      console.log (`${ingredient} is OUT of stock!`)
+    }
+    if(amount === 1) {
+      console.log (`${ingredient} is running LOW`)
+    }
+  
+  }
+  checkStock(4,"coffee");
+  checkStock(3,"tortillas");
+  checkStock(0,"Cheese")
+  checkStock(1, "Salsa")
+  //  all done
+  
--- selfEval.md
+++ selfEval.md
@@ -1,6 +1,13 @@
 # Self Evaluation
 
 - Reflect on your time management and awareness of where you spend time. Is this a strength or area you are working on? What support do you need in this area? And/or, what tips do you have to share with your classmates in this area?
+    I have been realizing that breaks are a necessity. If I just sit and stare at the computer without a break, I spiral and i'm totally unable to find
+    my way out. Then, I'm stuck on something way longer than I would have been had I stepped away and come back with a clear mind. Then I no longer have
+    the anxiety that blocks my ability to do my work.
 - How would you explain to a 5 year old what a function is?
+    I would tell them that a function is like a game. Every deck of cards has the same amount of jacks, queens, kings, and numbers. And every time you 
+    buy a deck of cards it's the same thing. The winner only wins buy playing buy the same rules every time.
 - Share your understanding of arguments and parameters.
+    arguments are the data and parameters are the place holders
 - What questions/topics (related to Functions) do you want to being to your Small Group?
+    When and how would you combine functions to other structers of code?
