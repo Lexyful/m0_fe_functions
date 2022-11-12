8f4c0c71cccfce28bb2e8e996a85d8faf3d1fd2c
Alexys Jordan <alexyskjordan@gmail.com>
2022-11-12 14:18:31 -0700

    Add cleanUp.js

--- cleanUp.js
+++ cleanUp.js
@@ -8,46 +8,43 @@
 
 
 // EX 1:
-// function nameQuestion() {
-//   console.log("Hello, what is your name?")
-// }
+function nameQuestion() {
+  console.log("Hello, what is your name?");
+}
 
-// nameQuestion()
-// // This exemple ran correctly in the terminal
-
+nameQuestion();
+// I added a semi colon after calling thing function
 
 // EX 2:
-// function addThreeNums(first, second, third) {
-// var sum = first + second + third;
-// console.log(sum);
-// }
 
-// addThreeNums(1, 2, 3);
-// addThreeNums(4, 2, 7);
-// I added the semi colon to keep the code consistent
+function addThreeNums(first, second, third) {
+  var sum = first + second + third
+  console.log(sum);
+}
 
+addThreeNums(1, 2, 3);
+addThreeNums(4, 2, 7);
+// I moved the curly bracket over, but this code ran correctly both ways
 
-// // EX 3:
-// function makeFreshPesto() {
-//   console.log("Buy ingredients: basil, parmesan, romano, olive oil, pine nuts, garlic, salt, pepper");
-//   console.log("Pulse basil and pine nuts");
-//   console.log("Add garlic and cheeses");
-//   console.log("Slowly pour in oil");
-//   console.log("Season");   
- 
-// }
+// EX 3:
+function makeFreshPesto() {
+  console.log("Buy ingredients: basil, parmesan, romano, olive oil, pine nuts, garlic, salt, pepper");
+  console.log("Pulse basil and pine nuts");
+  console.log("Add garlic and cheeses");
+  console.log("Slowly pour in oil");
+  console.log("Season");    
+}
 
-// makeFreshPesto();
-// I spelled funtion correctly and spaced the curly brackets where they should be. 
-// The bottom one needed to move down.
+makeFreshPesto();
+// I corrected the spelling of function and placed the second curly bracket under the console.log
 
+//  EX 4:
+function average(num1, num2) {
+var sum = 10 + 8;
+    var avg = sum / 2;
 
-// //  EX 4:
-// function average(num1, num2) 
-//   {
-// var sum = num1 + num2;
-//     var avg = sum / 2;
+  console.log(`the average is ${avg}.`)
+  }
 
-//   console.log(`the average is: ${avg}`)
-//   }
-//   I took out the period after ${avg}`k
+  average()
+  // I added numbers so that the average would give me a value in number form and I added average() at the end to call the function
